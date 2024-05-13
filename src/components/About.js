913ff19d2be06c5960ccd0d524aeebc11b4c4539
kinotikm <cloutkevin254@gmail.com>
2024-05-13 21:43:01 +0300

    commits

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,29 @@
 import React from "react";
+import Links from "./Links"
+function About(props) {
+  if (!props.bio || props.bio === "") {
+  
 
-function About() {
   return (
     <div id="about">
+    
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links />
     </div>
-  );
-}
+  );}
+  else {
+    return (
+      <div id = "about">
+        <h2>About Me</h2>
+        <p>{props.bio}</p>
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        <Links github = {props.links.github} />
+      </div>
+    )
+  }
+};
+
 
 export default About;
